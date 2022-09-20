45f1f778a849d9bf3daef93395c10fd07875ffdc
Niko <>
2022-09-20 13:10:30 +0800

    Refactor:Change the structure of directory. Feat:Update iOS sample and the source code of MRZ.

---
+++ ios/Objective-C/DynamsoftMRZRecognizerSDK/DynamsoftMRZRecognizerSDK/DynamsoftMRZRecognizerSDK.h
@@ -0,0 +1,15 @@
+//
+//  DynamsoftMRZRecognizerSDK.h
+//  DynamsoftMRZRecognizerSDK
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DynamsoftMRZRecognizerSDK.
+FOUNDATION_EXPORT double DynamsoftMRZRecognizerSDKVersionNumber;
+
+//! Project version string for DynamsoftMRZRecognizerSDK.
+FOUNDATION_EXPORT const unsigned char DynamsoftMRZRecognizerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamsoftMRZRecognizerSDK/PublicHeader.h>
+
+#import <DynamsoftMRZRecognizer/DynamsoftMRZRecognizer.h>
